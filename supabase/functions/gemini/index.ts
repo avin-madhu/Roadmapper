09169fce7037fe4bd252e19f3c7b4ad3524844be
Aadithya Sai <aadithyas0610@gmail.com>
2024-06-26 19:54:07 +0530

    Added supabase

---
+++ supabase/functions/gemini/index.ts
@@ -0,0 +1,34 @@
+// Setup type definitions for built-in Supabase Runtime APIs
+import "https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts";
+import { GoogleGenerativeAI } from "https://esm.run/@google/generative-ai";
+import { corsHeaders } from '../_shared/cors.ts';
+
+Deno.serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
+  }
+
+  const { query } = await req.json();
+  const apiKey = Deno.env.get("GOOGLE_GENAI_API_KEY");
+  const genai = new GoogleGenerativeAI(apiKey);
+
+  const prompt = `Generate a roadmap for the following: ${query}. The roadmap should be concise. You should return the goals as a javascript array of strings. do not use code blocks in the response`;
+
+  const model = genai.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const result = await model.generateContent(prompt);
+  try {
+    const reply = result.response.text();
+    console.log(reply)
+    // TODO: Handle cases when response is in invalid format (such as when gemini fails to generate content)
+    return new Response(reply, {
+      headers: { ...corsHeaders, "Content-Type": "text/plain" },
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response("", {
+      headers: { ...corsHeaders, "Content-Type": "text/plain" },
+      status: 500,
+    });
+  }
+});
